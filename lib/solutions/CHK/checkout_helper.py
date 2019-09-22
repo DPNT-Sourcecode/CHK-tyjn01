b87c46d3cc87ae2dfd9d1ab20ee5a92dceda20ea
sbx_user1051 <sbx_user1051@169.254.38.229>
2019-09-22 18:01:11 +0000

    Sun Sep 22 18:01:11 UTC 2019

--- lib/solutions/CHK/checkout_helper.py
+++ lib/solutions/CHK/checkout_helper.py
@@ -1,8 +1,16 @@
-from typing import List
+from typing import List, Union
+
 
 class CheckoutHelper:
 
     def __init__(self, items: List[dict]):
         self.items = {item['sku']: item for item in items}
 
-        self.offers = [item['offers'] for item in items if offer['offers']]
+        self.offers = [item['offers'] for item in items if item['offers']]
+
+    def get_item_by_sku(self, sku: str) -> Union[dict, None]:
+        return self.items.get(sku)
+
+    def get_items_by_skus_sorted_by_price(self, skus: List[str]) -> List[dict]:
+
+

5118e26dc7f334ba401298d3d2a2aff6a32707e1
sbx_user1051 <sbx_user1051@169.254.38.229>
2019-09-22 18:22:11 +0000

    Sun Sep 22 18:22:11 UTC 2019

--- lib/solutions/CHK/items.py
+++ lib/solutions/CHK/items.py
@@ -1,12 +1,5 @@
 from enum import Enum, auto
 
-
-class OfferTypes(Enum):
-    multi_price = auto()
-    get_one_free = auto()
-    group_buy_discount = auto()
-
-
 ITEMS = [
     {
         'sku': 'A',
@@ -276,4 +269,5 @@ ITEMS = [
         ]
     },
 ]
+
 

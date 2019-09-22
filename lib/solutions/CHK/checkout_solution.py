03c11e052448140e8ac75e97d5868847d4b842ef
sbx_user1051 <sbx_user1051@169.254.38.229>
2019-09-22 18:13:11 +0000

    Sun Sep 22 18:13:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,23 +41,7 @@ def apply_offers(sku_counts: Dict[str, int]) -> (Dict[str, int], int):
             sku_count = sku_counts[sku]
 
             if offer_type == OfferTypes.get_one_free:
-                if offer_details['free_gift'] == sku:
-                    # Need to take into account that you to give away an F you need some
-                    # the offer quantity + 1, for example if 4 F's purchased then only 1 can be
-                    # given away as you have 2F + 1 for free, and 1 at the normal price.
-                    num_multiples = sku_count // (offer_details['quantity'] + 1)
-                else:
-                    num_multiples = sku_count // offer_details['quantity']
 
-                sku_count_of_free_gift = sku_counts[offer_details['free_gift']]
-
-                sku_counts[offer_details['free_gift']] = max(sku_count_of_free_gift - num_multiples, 0)
-
-                # To prevent a given item being counted multiple times the sku count must be reduced
-                # by the number state in this offer, and then the "processed" items are simply added to
-                # the bill at their individual item price.
-                sku_counts[sku] -= num_multiples * offer_details['quantity']
-                total += (num_multiples * offer_details['quantity']) * ITEMS[sku]['unit_price']
 
             elif offer_type == OfferTypes.multi_price:
                 num_multiples = sku_count // offer_details['quantity']
@@ -84,18 +68,44 @@ def _apply_group_discount_offer(offer_details: dict, sku_counts: dict):
     num_multiples = qualifying_purchase_number // offer_details['quantity']
     subtotal += num_multiples * offer_details['price']
 
-    items_to_process = num_multiples * offer_details['quantity']
+    number_of_items_used_in_offer = num_multiples * offer_details['quantity']
 
     # because we are fair to the customers we include expensive items first:
     for item in checkout_helper.get_items_by_skus_sorted_by_price(offer_details['skus']):
         item_sku_count = sku_counts[item['sku']]
 
-        number_to_reduce_by = min(item_sku_count, items_to_process)
-        items_to_process -= number_to_reduce_by
-        sku_counts[sku] -= number_to_reduce_by
+        num_used_in_offer = min(item_sku_count, number_of_items_used_in_offer)
 
-        if not items_to_process:
+        number_of_items_used_in_offer -= num_used_in_offer
+        sku_counts[item['sku']] -= num_used_in_offer
+
+        if not number_of_items_used_in_offer:
             break
+
+    return sku_counts, subtotal
+
+def _apply_get_one_free_offer(offer_details: dict, sku_counts: dict):
+    sku = offer_details['sku']
+    sku_count = sku_counts[sku]
+
+    if offer_details['free_gift'] == sku:
+        # Need to take into account that you to give away an F you need some
+        # the offer quantity + 1, for example if 4 F's purchased then only 1 can be
+        # given away as you have 2F + 1 for free, and 1 at the normal price.
+        num_multiples = sku_count // (offer_details['quantity'] + 1)
+    else:
+        num_multiples = sku_count // offer_details['quantity']
+
+    sku_count_of_free_gift = sku_counts[offer_details['free_gift']]
+
+    sku_counts[offer_details['free_gift']] = max(sku_count_of_free_gift - num_multiples, 0)
+
+    # To prevent a given item being counted multiple times the sku count must be reduced
+    # by the number state in this offer, and then the "processed" items are simply added to
+    # the bill at their individual item price.
+    sku_counts[sku] -= num_multiples * offer_details['quantity']
+
+    subtotal += (num_multiples * offer_details['quantity']) * ITEMS[sku]['unit_price']
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -112,7 +122,9 @@ def checkout(skus: str) -> int:
         offer_type = offer_details['offer_type']
 
         if offer_type == OfferTypes.group_buy_discount:
-            _apply_group_discount_offer(offer_details, sku_counts)
+            sku_counts, subtotal = _apply_group_discount_offer(offer_details, sku_counts)
+        elif offer_type == OfferTypes.get_one_free:
+
 
 
 
@@ -121,6 +133,7 @@ def checkout(skus: str) -> int:
         total += count * ITEMS[sku]['unit_price']
 
     return total
+
 
 
 
